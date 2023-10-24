162bdf6d04f35c3545a9eea281c608585bf9a8ba
duanhongyi <duanhongyi@doopai.com>
2023-10-24 10:15:21 +0800

    chore(registry): change registry port to 5000

--- contrib/ci/test.sh
+++ contrib/ci/test.sh
@@ -2,13 +2,16 @@
 
 set -eoxf pipefail
 
-s3Accesskey=drycc
-s3Secretkey=123456789
+BASE_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+DRYCC_STORAGE_ACCESSKEY=f4c4281665bc11ee8e0400163e04a9cd
+DRYCC_STORAGE_SECRETKEY=f4c4281665bc11ee8e0400163e04a9cd
+
 
 STORAGE_JOB=$(podman run -d --entrypoint init-stack -p 8333:8333 \
-  -e DRYCC_STORAGE_ACCESSKEY=$s3Accesskey \
-  -e DRYCC_STORAGE_SECRETKEY=$s3Secretkey \
-  "${DEV_REGISTRY}"/drycc/storage:canary weed server -dir=/data/hdd -s3)
+  -v "${BASE_DIR}":/tmp/weed \
+  -e DRYCC_STORAGE_ACCESSKEY="${DRYCC_STORAGE_ACCESSKEY}" \
+  -e DRYCC_STORAGE_SECRETKEY="${DRYCC_STORAGE_SECRETKEY}" \
+  "${DEV_REGISTRY}"/drycc/storage:canary /tmp/weed/start-s3.sh)
 
 # wait for port
 STORAGE_IP=$(podman inspect --format "{{ .NetworkSettings.IPAddress }}" "${STORAGE_JOB}")
@@ -17,7 +20,7 @@ wait-for-port --host="${STORAGE_IP}" 8333
 echo -e "\\033[32m---> S3 service ${STORAGE_IP}:8333 ready...\\033[0m"
 podman logs "${STORAGE_JOB}"
 
-JOB=$(podman run -d -p 5000:5000 \
+JOB=$(podman run -d \
   -e REGISTRY_HTTP_SECRET=drycc \
   -e DRYCC_REGISTRY_REDIRECT=false \
   -e DRYCC_REGISTRY_USERNAME=admin \
@@ -25,8 +28,8 @@ JOB=$(podman run -d -p 5000:5000 \
   -e DRYCC_STORAGE_LOOKUP=path \
   -e DRYCC_STORAGE_BUCKET=registry \
   -e DRYCC_STORAGE_ENDPOINT="http://${STORAGE_IP}:8333" \
-  -e DRYCC_STORAGE_ACCESSKEY=$s3Accesskey \
-  -e DRYCC_STORAGE_SECRETKEY=$s3Secretkey \
+  -e DRYCC_STORAGE_ACCESSKEY="${DRYCC_STORAGE_ACCESSKEY}" \
+  -e DRYCC_STORAGE_SECRETKEY="${DRYCC_STORAGE_SECRETKEY}" \
   "$1")
 
 # shellcheck disable=SC2317
@@ -34,7 +37,7 @@ function clean_before_exit {
   # delay before exiting, so stdout/stderr flushes through the logging system
   podman kill "${JOB}"
   podman kill "${STORAGE_JOB}"
-  podman rm "${JOB}" "${STORAGE_JOB}"
+  podman rm -f "${JOB}" "${STORAGE_JOB}"
 }
 trap clean_before_exit EXIT
 
